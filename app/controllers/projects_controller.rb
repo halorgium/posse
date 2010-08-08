5d6cc4f5b7737259e301d5ed446152d3140001a0
Tim Carey-Smith <tim@spork.in>
2010-08-08 00:35:59 -0700

    Add more views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,25 @@
 class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+    render
+  end
+
+  def new
+    @project = Project.new
+    render
+  end
+
+  def create
+    @project = Project.new(params[:project])
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @project = Project.by_param(params[:id])
+    render
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,11 @@
 class Project < ActiveRecord::Base
+  def self.by_param(param)
+    where(:name => param).first
+  end
+
+  validates_uniqueness_of :name
+
+  def to_param
+    name
+  end
 end
