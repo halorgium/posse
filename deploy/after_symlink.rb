c89ba1a18674d16d7123afebf04a80154d2cbf08
Tim Carey-Smith <tim@spork.in>
2010-08-08 07:15:50 -0700

    Make and Symlink checkouts dir

--- deploy/after_symlink.rb
+++ deploy/after_symlink.rb
@@ -1,1 +1,4 @@
 run "ln -nfs #{shared_path}/config/resque.yml #{latest_release}/config/resque.yml"
+
+run "[ -f #{shared_path}/tmp/checkouts ] || mkdir -p #{shared_path}/tmp/checkouts"
+run "ln -nfs #{shared_path}/tmp/checkouts #{latest_release}/tmp/checkouts"
