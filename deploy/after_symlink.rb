68f534e73382be4f13acfa8d9a038507ba5b2017
Tim Carey-Smith <tim@spork.in>
2010-08-08 07:16:48 -0700

    Symlink git ssh keys

--- deploy/after_symlink.rb
+++ deploy/after_symlink.rb
@@ -1,4 +1,7 @@
 run "ln -nfs #{shared_path}/config/resque.yml #{latest_release}/config/resque.yml"
 
+run "ln -nfs #{shared_path}/config/git.id_rsa     #{latest_release}/config/git.id_rsa"
+run "ln -nfs #{shared_path}/config/git.id_rsa.pub #{latest_release}/config/git.id_rsa.pub"
+
 run "[ -f #{shared_path}/tmp/checkouts ] || mkdir -p #{shared_path}/tmp/checkouts"
 run "ln -nfs #{shared_path}/tmp/checkouts #{latest_release}/tmp/checkouts"
