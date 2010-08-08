e4b21be0c68d3beef418d456dcb2eecc29060ed4
Tim Carey-Smith <tim@spork.in>
2010-08-08 01:58:09 -0700

    Create branches as commits come in

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,3 +1,7 @@
 class Branch < ActiveRecord::Base
   belongs_to :project
+
+  def build(payload)
+    # TODO
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -5,11 +5,23 @@ class Project < ActiveRecord::Base
 
   def self.handle(payload)
     Rails.logger.info "Got post-receive: #{payload.inspect}"
+    if project = where(:git_uri => payload.uri).first
+      project.build(payload)
+    end
   end
 
   validates_uniqueness_of :name
 
   has_many :branches
+
+  def build(payload)
+    unless branch = branches.where(:name => payload.branch).first
+      branch = branches.new(:name => payload.branch)
+      branch.save!
+    end
+
+    branch.build(payload)
+  end
 
   def to_param
     name
