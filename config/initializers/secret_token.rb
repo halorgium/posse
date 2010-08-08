1984ef28d3ce9e8c21ea9cf3fbade695e9c94c7c
Tim Carey-Smith <tim@spork.in>
2010-08-07 21:54:06 -0700

    Initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Posse::Application.config.secret_token = '304c6e09d2bdf26bc437d538e9516637468a5d91442570afa082195a1dc749575e948d4246b75cc2d2ecedf547c2a1be63574c9c7bf8e8009bdea24caa2755e0'
