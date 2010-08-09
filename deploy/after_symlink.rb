924aaec90811b53095da1d190b800447521f5216
Tim Carey-Smith <tim@spork.in>
2010-08-08 19:30:43 -0700

    Symlink deploy keys

--- deploy/after_symlink.rb
+++ deploy/after_symlink.rb
@@ -3,5 +3,8 @@ run "ln -nfs #{shared_path}/config/resque.yml #{latest_release}/config/resque.ym
 run "ln -nfs #{shared_path}/config/git.id_rsa     #{latest_release}/config/git.id_rsa"
 run "ln -nfs #{shared_path}/config/git.id_rsa.pub #{latest_release}/config/git.id_rsa.pub"
 
+run "ln -nfs #{shared_path}/config/deploy.id_rsa     #{latest_release}/config/deploy.id_rsa"
+run "ln -nfs #{shared_path}/config/deploy.id_rsa.pub #{latest_release}/config/deploy.id_rsa.pub"
+
 run "[ -f #{shared_path}/tmp/checkouts ] || mkdir -p #{shared_path}/tmp/checkouts"
 run "ln -nfs #{shared_path}/tmp/checkouts #{latest_release}/tmp/checkouts"
